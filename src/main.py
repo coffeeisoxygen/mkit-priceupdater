a074dd67d0805f409df7b437095f52f3ffd2a317
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-09-10 12:16:48 +0700

    refactor: move logging setup to main function

--- src/main.py
+++ src/main.py
@@ -8,12 +8,13 @@ from mlogging.setup import setup_logging
 settings = get_settings()
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 LOG_CONFIG_PATH = BASE_DIR / "log_config.yaml"
+setup_logging(config_path=LOG_CONFIG_PATH, env=settings.APP.ENVIRONMENT)
 
 
 def main():
-    setup_logging(config_path=LOG_CONFIG_PATH, env=settings.APP.ENVIRONMENT)
-    for key, value in settings.APP.model_dump().items():
-        logger.bind(env=settings.APP.ENVIRONMENT).debug(f"{key}: {value}")
+    logger.bind(env=settings.APP.ENVIRONMENT).debug(
+        f"APP settings: {settings.APP.model_dump()}"
+    )
 
 
 if __name__ == "__main__":
